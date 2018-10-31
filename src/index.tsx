42771c230927f047ca1046d5ac6eaf644fb01fb6
fragment0 <hi@fragment0.com>
2018-10-31 19:56:30 +0800

    seperate pro & dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,113 +1,13 @@
-import * as React from 'react'
-import {findDOMNode} from 'react-dom'
-import {findAllInRenderedTree} from 'react-dom/test-utils'
+import {ReactElement} from 'react'
+import {persist as devPersist} from './persist'
 
-export interface CachedComponentProps<P> {
-  cache: {
-    data: P,
-    update(data: P): void,
+export type Persist = () => (element: ReactElement<any>) => ReactElement<any>
+
+const proPersist: Persist = () => {
+  return (element: ReactElement<any>) => {
+    return element
   }
 }
 
-function subEqual(a: any, b: any): boolean {
-  if (!a && !b) return true
-  if (!a || !b) return false
-
-  const na: any[] = []
-  const nb: any[] = []
-
-  Object.entries(a).forEach(([key, value]) => {
-    if (typeof b[key] !== undefined) {
-      na.push([key, value])
-      nb.push([key, b[key]])
-    }
-  })
-  return JSON.stringify(na) === JSON.stringify(nb)
-}
-
-function getComponentFingerprint(c: React.Component): string {
-  return (c as any)._reactInternalFiber.index.toString()
-}
-
-function findStateFulComponent(root: React.Component): React.Component[] {
-  return findAllInRenderedTree(root, 
-    i => i && i instanceof React.Component && Boolean(i.state)) as React.Component[]
-}
-
-function saveStatefulComponent(targetMap: Map<any, any>, root: React.Component) {
-  targetMap.clear()
-  const result = findStateFulComponent(root)
-
-  result.forEach(c => {
-    // TODO instance fingerprint
-    // temp use index
-    targetMap.set(getComponentFingerprint(c), c.state)
-  })
-}
-
-export function cache() {
-  let cached: any = null
-  const cachedMap: Map<string, any> = new Map()
-
-  class CachedWrapper extends React.Component {
-    node: React.Component | null = null
-    observer: MutationObserver | null = null
-    needReset = false
-    lock = false // lock research state
-
-    componentDidMount() {
-      const el = findDOMNode(this.node!)
-
-      const observer = this.observer = new MutationObserver(this.changeCallback)
-      observer.observe(el!.parentNode!, {attributes: true, childList: true, 
-        subtree: true, characterData: true})
-
-      cachedMap.set(getComponentFingerprint(this.node!), cached)
-      saveStatefulComponent(cachedMap, this.node!)
-    }
-
-    // only run when hot reload
-    componentDidUpdate() {
-      // temp lock for prevent changeCallback
-      this.lock = true
-
-      const updated = findStateFulComponent(this.node!)
-      updated.forEach(c => {
-        const fingerprint = getComponentFingerprint(c)
-        const state = cachedMap.get(fingerprint)
-        if (state) {
-          // TODO make async
-          c.setState(state)
-        }
-      })
-
-      requestAnimationFrame(() => {
-        this.lock = false
-      })
-    }
-
-    componentWillUnmount() {
-      if (this.observer) {
-        this.observer.disconnect()
-      }
-    }
-
-    changeCallback = () => {
-      if (!this.lock) {
-        saveStatefulComponent(cachedMap, this.node!)
-      }
-    }
-
-    render() {
-      return React.cloneElement(this.props.children as React.ReactElement<any>, {
-        // definitely is Component
-        ref: (el: React.Component) => { this.node = el },
-      })
-    }
-  }
-
-  return (element: React.ReactElement<any>) => {
-    if (process.env.NODE_ENV === 'production') return element
-    return (<CachedWrapper>{element}</CachedWrapper>)
-  }
-}
+export const persist = process.env.NODE_ENV === 'production' ?
+  proPersist : devPersist
---
+++ src/persist.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react'
+import {findDOMNode} from 'react-dom'
+import {findAllInRenderedTree} from 'react-dom/test-utils'
+import {Persist} from '.'
+
+/* no way to get a persistent fingerprint for each render
+function getComponentFingerprint(c: React.Component): string {
+  const fiberNode = (c as any)._reactInternalFiber
+
+  const result = [fiberNode.index]
+  let child = fiberNode.child
+  while (child) {
+    result.push(child.index)
+    child = child.child
+  }
+  return result.join('.')
+}
+ */
+function findStateFulComponent(root: React.Component): React.Component[] {
+  return findAllInRenderedTree(root,
+    i => i && i instanceof React.Component && Boolean(i.state)) as React.Component[]
+}
+
+function saveStatefulComponent(targetMap: Map<any, any>, root: React.Component) {
+  targetMap.clear()
+  const result = findStateFulComponent(root)
+
+  result.forEach((c, index) => {
+    // TODO instance fingerprint
+    // temp use index
+    // targetMap.set(getComponentFingerprint(c, index), c.state)
+    targetMap.set(index, c.state)
+  })
+}
+
+function updateComponentWithCache(root: React.Component, data: Map<any, any>, plus: number, callback: () => void) {
+  const targets = findStateFulComponent(root)
+  if (targets[plus]) {
+    targets[plus].setState(data.get(plus), () => {
+      updateComponentWithCache(root, data, plus + 1, callback)
+    })
+  } else {
+    callback()
+  }
+
+}
+
+export const persist: Persist = () => {
+  const persistMap: Map<number, any> = new Map()
+
+  class PersistWrapper extends React.Component {
+    node: React.Component | null = null
+    observer: MutationObserver | null = null
+    needReset = false
+    lock = false // lock research state
+
+    componentDidMount() {
+      const el = findDOMNode(this.node!)
+      const observer = this.observer = new MutationObserver(this.changeCallback)
+      observer.observe(el!.parentNode!, {attributes: true, childList: true,
+        subtree: true, characterData: true})
+
+      saveStatefulComponent(persistMap, this.node!)
+    }
+
+    // only run when hot reload
+    componentDidUpdate() {
+      // temp lock for prevent changeCallback
+      this.lock = true
+
+      updateComponentWithCache(this.node!, persistMap, 0, () => {
+        requestAnimationFrame(() => {
+          this.lock = false
+        })
+      })
+    }
+
+    componentWillUnmount() {
+      if (this.observer) {
+        this.observer.disconnect()
+      }
+    }
+
+    changeCallback = () => {
+      if (!this.lock) {
+        saveStatefulComponent(persistMap, this.node!)
+      }
+    }
+
+    render() {
+      // definitely is Component
+      return React.cloneElement(React.Children.only(this.props.children), {
+        ref: (el: React.Component) => { this.node = el },
+      })
+    }
+  }
+
+  return (element: React.ReactElement<any>) => {
+    return (<PersistWrapper>{element}</PersistWrapper>)
+  }
+}
